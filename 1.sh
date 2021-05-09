fcadc8d55750137154a1c0c313f4efe730bcb0ab
RominaKavi <Romishkavi@gmail.com>
2021-05-09 16:55:14 +0430

    massage

---
+++ 1.sh
@@ -0,0 +1,8 @@
+ls
+Desktop    Music     Public  Romina      Templates
+Documents  Pictures  rom     Romina.txt  Videos
+ls -s
+total 40
+4 Desktop    4 Music     4 Public  4 Romina      4 Templates
+4 Documents  4 Pictures  4 rom     4 Romina.txt  4 Videos
+
